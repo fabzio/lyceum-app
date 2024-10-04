48ebe3aa39a628d8e41aaff5dcb93e401402057d
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-10-04 16:14:41 -0500

    refactor(layout): :recycle: abstraer componente de layaout por página

--- src/security/index.tsx
+++ src/security/index.tsx
@@ -1,14 +1,10 @@
+import PageLayout from '@/layouts/PageLayout'
 import SecurityManagement from './components/SecurityManagement'
 
 export default function Security() {
   return (
-    <div>
-      <div className="p-6">
-        <h2 className="text-4xl font-bold">Seguridad</h2>
-      </div>
-      <section className="w-4/5 md:w-3/5 max-w-6xl mx-auto">
-        <SecurityManagement />
-      </section>
-    </div>
+    <PageLayout name="Seguridad">
+      <SecurityManagement />
+    </PageLayout>
   )
 }
