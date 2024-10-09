a0abf99f199ca97a6f87445345705559a67a12cb
ribartra <69020755+ribartra@users.noreply.github.com>
2024-10-09 05:37:24 -0500

    feat: construir plan

---
+++ src/routes/plan-de-estudios.tsx
@@ -0,0 +1,15 @@
+import TransitionPage from '@/components/anim/TransitionPage'
+import StudyPlans from '@/study-plans'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/plan-de-estudios')({
+  component: () => <StudyPlansPage />,
+})
+
+function StudyPlansPage() {
+  return (
+    <TransitionPage>
+      <StudyPlans />
+    </TransitionPage>
+  )
+}
---
+++ src/study-plans/index.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/layouts/PageLayout'
+import StudyPlansManagment from './components/StudyPlansManagment'
+
+export default function StudyPlans() {
+  return (
+    <PageLayout name="Plan de estudios">
+      <StudyPlansManagment />
+    </PageLayout>
+  )
+}
