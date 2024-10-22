81eda889edf0c9647a9c719e622cec50f06e5a7a
misahor67 <99337812+misahor67@users.noreply.github.com>
2024-10-22 14:56:37 -0500

    feat(Students): :sparkles: agregar service

--- src/modules/accounts/services/Student.service.ts
+++ src/modules/accounts/services/Student.service.ts
@@ -1,9 +1,29 @@
 import db from '@/database'
-import { accounts } from '@/database/schema'
-import { eq } from 'drizzle-orm'
+import { accounts, units } from '@/database/schema'
+import { eq, sql } from 'drizzle-orm'
+import { StudentDAO } from '../daos/StudentDAO'
 
-class StudentService {
-  //TODO Add methods here
+class StudentService implements StudentDAO {
+  async getStudentDetail(params: { code: string }) {
+    const student = await db
+      .select({
+        code: accounts.code,
+        name: accounts.name,
+        firstSurname: accounts.firstSurname,
+        secondSurname: accounts.secondSurname,
+        email: accounts.email,
+        state: accounts.state,
+        speciallity: units.name,
+      })
+      .from(accounts)
+      .innerJoin(units, eq(units.id, accounts.unitId))
+      .where(eq(accounts.code, params.code))
+    if (student.length === 0) {
+      throw new Error('Student not found')
+    }
+    const [studentDetail] = student
+    return studentDetail
+  }
 }
 
 export default StudentService
