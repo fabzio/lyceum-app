5076df54f4fef29cc62820c0bd80d523a5ad80a8
misahor67 <99337812+misahor67@users.noreply.github.com>
2024-10-22 14:53:25 -0500

    feat(Students): :sparkles: agregar ruta

--- src/modules/accounts/routes/Student.route.ts
+++ src/modules/accounts/routes/Student.route.ts
@@ -1,16 +1,18 @@
 import { Route } from '@/interfaces/route'
 import { Hono } from 'hono'
+import { StudentController } from '../controllers'
 
 class StudentRoute implements Route {
   public path = '/students'
   public router = new Hono()
+  private studenController = new StudentController()
 
   constructor() {
     this.initializeRoutes()
   }
 
   private initializeRoutes() {
-    // TODO Add route initializations here
+    this.router.route('/', this.studenController.getStudentDetail)
   }
 }
 export default StudentRoute
