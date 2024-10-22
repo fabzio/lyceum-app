03cbeaa4e33cfefacf42de5140e1341e1ca05786
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-10-22 01:28:34 -0500

    style(accounts): :truck: agregar servicios

---
+++ src/modules/accounts/services/index.ts
@@ -0,0 +1,4 @@
+export { default as StudentService } from './Student.service'
+export { default as ProfessorService } from './Professor.service'
+export { default as AdministrativeService } from './Administrative.service'
+export { default as ExternalService } from './External.service'
