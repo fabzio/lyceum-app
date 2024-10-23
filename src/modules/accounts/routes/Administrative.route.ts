8d9cf62bb3a28f9530d4ede45f6b71b26878e43b
LeonardoArdo <a20197102@pucp.edu.pe>
2024-10-23 17:12:06 -0500

    feat(Administrator): :sparkles: agregando ruta

--- src/modules/accounts/routes/Administrative.route.ts
+++ src/modules/accounts/routes/Administrative.route.ts
@@ -1,16 +1,22 @@
 import { Route } from '@/interfaces/route'
 import { Hono } from 'hono'
+import { AdministrativeController } from '../controllers'
 
 class AdministrativeRoute implements Route {
   public path = '/admins'
   public router = new Hono()
+  public administrativeController = new AdministrativeController()
 
   constructor() {
     this.initializeRoutes()
   }
 
   private initializeRoutes() {
-    //TODO Add route initializations here
+    //Acá colocamos todas las funciones que tiene el controlador de administrativos
+    this.router.route(
+      '/',
+      this.administrativeController.getAdministrativeDetail
+    )
   }
 }
 
