e8ff0a575875cf838a4b007580d3ed501fe079a4
Paul Domingo Espettia Rodríguez <paul.espettia@pucp.edu.pe>
2024-10-24 01:36:49 -0500

    feat(students): agregar declaracion de controller para getStudents con mockdata para probar el front

--- src/modules/accounts/controllers/Student.controller.ts
+++ src/modules/accounts/controllers/Student.controller.ts
@@ -34,6 +34,82 @@ class StudentController {
       }
     }
   )
+
+  public getStudents = this.router.get(
+    '/',
+    zValidator(
+      'query',
+      z.object({
+        q: z.string().optional(),
+        page: z.string().transform((v) => parseInt(v)),
+        limit: z.string().transform((v) => parseInt(v)),
+        sortBy: z.string().optional(),
+      })
+    ),
+    async (c) => {
+      try {
+        const response: ResponseAPI = {
+          //TODO: Cambiar el mock data por la llamada al servicio
+          data: {
+            "result": [
+              {
+                  "code": "ACC001",
+                  "name": "John",
+                  "firstSurname": "Doe",
+                  "secondSurname": "Smith",
+                  "email": "john.doe@example.com",
+                  "state": "active"
+              },
+              {
+                  "code": "ACC002",
+                  "name": "Jane",
+                  "firstSurname": "Williams",
+                  "secondSurname": "Brown",
+                  "email": "jane.williams@example.com",
+                  "state": "inactive"
+              },
+              {
+                  "code": "ACC003",
+                  "name": "Alice",
+                  "firstSurname": "Johnson",
+                  "secondSurname": "Miller",
+                  "email": "alice.johnson@example.com",
+                  "state": "active"
+              },
+              {
+                  "code": "ACC004",
+                  "name": "Bob",
+                  "firstSurname": "Davis",
+                  "secondSurname": "Garcia",
+                  "email": "bob.davis@example.com",
+                  "state": "pending"
+              },
+              {
+                  "code": "ACC005",
+                  "name": "Charlie",
+                  "firstSurname": "Martinez",
+                  "secondSurname": "Lopez",
+                  "email": "charlie.martinez@example.com",
+                  "state": "active"
+              }
+          ],
+            "rowCount": 29,
+            "currentPage": 0,
+            "totalPages": 6,
+            "hasNext": true
+        }, // mock data
+          success: true,
+          message: 'Students retrived',
+        }
+        return c.json(response)
+      } catch (error) {
+        if (error instanceof LyceumError) {
+          c.status(error.code)
+        }
+        throw error
+      }
+    }
+  )
 }
 
 export default StudentController
