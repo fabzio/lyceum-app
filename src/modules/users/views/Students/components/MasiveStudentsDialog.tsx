4c965e35db1e8375bbad87d34ebd3732e7bfecfb
Paul Domingo Espettia Rodríguez <paul.espettia@pucp.edu.pe>
2024-10-24 01:15:18 -0500

    feat(estudiantes): agregar vista de datatable

--- src/modules/users/views/Students/components/MasiveStudentsDialog.tsx
+++ src/modules/users/views/Students/components/MasiveStudentsDialog.tsx
@@ -1,116 +1,115 @@
-// TODO: Adaptar el siguiente código para que funcione con la entidad de Estudiantes
-// import { Button } from '@/components/ui/button'
-// import {
-//   Dialog,
-//   DialogClose,
-//   DialogContent,
-//   DialogDescription,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-// } from '@/components/ui/dialog'
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from '@/components/ui/form'
-// import { Input } from '@/components/ui/input'
-// import { QueryKeys } from '@/constants/queryKeys'
-// import { useToast } from '@/hooks/use-toast'
-// import { Course } from '@/interfaces/models/Course'
-// import { getCsvData } from '@/lib/utils'
-// import CourseService from '@/modules/study-plans/services/course.service'
-// import { zodResolver } from '@hookform/resolvers/zod'
-// import { useMutation, useQueryClient } from '@tanstack/react-query'
-// import { Loader2, Upload } from 'lucide-react'
-// import { useState } from 'react'
-// import { useForm } from 'react-hook-form'
-// import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { QueryKeys } from '@/constants/queryKeys'
+import { useToast } from '@/hooks/use-toast'
+import { Course } from '@/interfaces/models/Course'
+import { getCsvData } from '@/lib/utils'
+import CourseService from '@/modules/study-plans/services/course.service'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Loader2, Upload } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-// export default function MasiveCoursesDialog() {
-//   const [isOpen, setIsOpen] = useState(false)
-//   const queryClient = useQueryClient()
-//   const { toast } = useToast()
-//   const form = useForm<z.infer<typeof formSchema>>({
-//     resolver: zodResolver(formSchema),
-//   })
-//   const { mutate, isPending } = useMutation({
-//     mutationFn: CourseService.addCourse,
-//     onSuccess: () => {
-//       queryClient.invalidateQueries({
-//         queryKey: [QueryKeys.studyPlan.COURSES],
-//       })
-//       setIsOpen(false)
-//     },
-//     onError: ({ message }) => {
-//       toast({
-//         title: 'Error',
-//         variant: 'destructive',
-//         description: message,
-//       })
-//     },
-//   })
+export default function MasiveCoursesDialog() {
+  const [isOpen, setIsOpen] = useState(false)
+  const queryClient = useQueryClient()
+  const { toast } = useToast()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+  const { mutate, isPending } = useMutation({
+    mutationFn: CourseService.addCourse,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.studyPlan.COURSES],
+      })
+      setIsOpen(false)
+    },
+    onError: ({ message }) => {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: message,
+      })
+    },
+  })
 
-//   const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-//     const dataJson = await getCsvData<
-//       Pick<Course, 'code' | 'credits' | 'name'>
-//     >(data.file)
-//     mutate(dataJson)
-//   }
-//   return (
-//     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-//       <DialogTrigger asChild>
-//         <Button variant="secondary">
-//           <Upload className="mr-2 h-4 w-4" /> Carga masiva
-//         </Button>
-//       </DialogTrigger>
-//       <DialogContent>
-//         <DialogHeader>
-//           <DialogTitle>Importar cursos desde archivo</DialogTitle>
-//           <DialogDescription>
-//             Sube un archivo CSV con los cursos que desea importar
-//           </DialogDescription>
-//         </DialogHeader>
-//         <Form {...form}>
-//           <form onSubmit={form.handleSubmit(handleSubmit)}>
-//             <FormField
-//               name="file"
-//               render={({ field: { value, onChange, ...filedProps } }) => (
-//                 <FormItem>
-//                   <FormLabel>Archivo</FormLabel>
-//                   <FormControl>
-//                     <Input
-//                       {...filedProps}
-//                       type="file"
-//                       accept=".csv"
-//                       onChange={(e) =>
-//                         onChange(e.target.files && e.target.files[0])
-//                       }
-//                     />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-//             <DialogFooter className="mt-2">
-//               <DialogClose>
-//                 <Button variant="secondary">Cancelar</Button>
-//               </DialogClose>
-//               <Button type="submit" disabled={isPending}>
-//                 {isPending ? <Loader2 className="animate-spin" /> : 'Importar'}
-//               </Button>
-//             </DialogFooter>
-//           </form>
-//         </Form>
-//       </DialogContent>
-//     </Dialog>
-//   )
-// }
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    const dataJson = await getCsvData<
+      Pick<Course, 'code' | 'credits' | 'name'>
+    >(data.file)
+    mutate(dataJson)
+  }
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button variant="secondary">
+          <Upload className="mr-2 h-4 w-4" /> Carga masiva
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Importar cursos desde archivo</DialogTitle>
+          <DialogDescription>
+            Sube un archivo CSV con los cursos que desea importar
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <FormField
+              name="file"
+              render={({ field: { value, onChange, ...filedProps } }) => (
+                <FormItem>
+                  <FormLabel>Archivo</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...filedProps}
+                      type="file"
+                      accept=".csv"
+                      onChange={(e) =>
+                        onChange(e.target.files && e.target.files[0])
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="mt-2">
+              <DialogClose>
+                <Button variant="secondary">Cancelar</Button>
+              </DialogClose>
+              <Button type="submit" disabled={isPending}>
+                {isPending ? <Loader2 className="animate-spin" /> : 'Importar'}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
 
-// const formSchema = z.object({
-//   file: z.instanceof(File, { message: 'Debe seleccionar un archivo' }),
-// })
+const formSchema = z.object({
+  file: z.instanceof(File, { message: 'Debe seleccionar un archivo' }),
+})
