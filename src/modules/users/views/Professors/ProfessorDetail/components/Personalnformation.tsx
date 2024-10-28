d85d108dca7fddea62c6b2951c790aedd4b41129
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-10-27 19:45:39 -0500

    feat(professor-detail): :sparkles: agregar detalle de profesor

---
+++ src/modules/users/views/Professors/ProfessorDetail/components/Personalnformation.tsx
@@ -0,0 +1,62 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { useFormContext } from 'react-hook-form'
+
+export default function Personalnformation() {
+  const form = useFormContext()
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Información Personal</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <FormField
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombres</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="firstSurname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Apellido Paterno</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="secondSurname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Apellido Materno</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </Form>
+      </CardContent>
+    </Card>
+  )
+}
