5f7577b22e191979080526ce964af4ab938e4ed9
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-10-31 11:20:22 -0500

    refactor: :alien: agregar validacion de URL de produccion

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,3 @@
 export const {
   VITE_API_URL: API_URL,
-  VITE_GOOGLE_CLIENT_ID: G_CLIENT_ID,
-  VITE_SECRET_KEY: SECRET_KEY,
 } = import.meta.env
--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,9 @@
+import { API_URL } from '@/config'
 import axios from 'axios'
 
 // Crear instancia de Axios con basePath
 const http = axios.create({
-  baseURL: '/api/v1',
+  baseURL: `${process.env.NODE_ENV === 'production' ? API_URL : ''}/api/v1`,
   timeout: 5000,
 })
 
