22e480b5f9ce7a901bb9462c86fb193b234d2ad8
thegodmenthat <129998847+thegodmenthat@users.noreply.github.com>
2024-11-02 00:45:29 -0500

    feat: implementar servicio en front del actualizar estado de una propuesta
    Ademas se añadio como opcional un parametro del dto en el backend de la misma propuesta

--- src/modules/enrollment/dtos/scheduleProposalDTO.ts
+++ src/modules/enrollment/dtos/scheduleProposalDTO.ts
@@ -15,14 +15,16 @@ export type InsertCourseToSchPropDTO = z.infer<typeof insertCourseToSchPropDTO>
 
 export const updateScheduleProposalStatusDTO = z.object({
   newStatus: z.enum(['requested', 'sended', 'aproved', 'assigned']),
-  coursesList: z.array(
-    z.object({
-      courseId: z.number().min(1),
-      vacanciesPerSchema: z.number().max(80).min(1),
-      visibleSchedules: z.number().max(10).min(1),
-      hiddenSchedules: z.number().max(10).min(1),
-    }),
-  )
+  coursesList: z
+    .array(
+      z.object({
+        courseId: z.number().min(1),
+        vacanciesPerSchema: z.number().max(80).min(1),
+        visibleSchedules: z.number().max(10).min(1),
+        hiddenSchedules: z.number().max(10).min(1),
+      })
+    )
+    .optional(),
 })
 
 export type UpdateScheduleProposalStatusDTO = z.infer<
