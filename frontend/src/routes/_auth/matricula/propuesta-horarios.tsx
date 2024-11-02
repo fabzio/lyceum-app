c0aca1ac15b95f49ddd943dcba13c45a48ea66ba
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-11-02 18:34:14 -0500

    feat(auth): :passport_control: agregar permisos para gestión de propuesta de horarios

--- frontend/src/routes/_auth/matricula/propuesta-horarios.tsx
+++ frontend/src/routes/_auth/matricula/propuesta-horarios.tsx
@@ -1,31 +1,41 @@
 import { QueryKeys } from '@frontend/constants/queryKeys'
+import { EnrollmentPermissionsDict } from '@frontend/interfaces/enums/permissions/Enrollment'
+import { haveSomePermission } from '@frontend/lib/utils'
+import { EnrollmentModule } from '@frontend/modules/enrollment/enrollment.module'
 import EnrollmentService from '@frontend/modules/enrollment/services/enrollment.service'
 import EnrollmentPropose from '@frontend/modules/enrollment/views/EnrollmentPropose'
 import { EnrollmentProposalFilters } from '@frontend/modules/enrollment/views/EnrollmentPropose/interfaces/EnrollmentProposalFilters'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth/matricula/propuesta-horarios')({
-  // TODO: Implementar persmisos para la propuesta de horarios
-  // beforeLoad: ({ context: { sessionStore } }) => {
-  //   const { getAllPermissions } = sessionStore
-  //   if (
-  //     !haveSomePermission(getAllPermissions(), [
-  //       EnrollmentPermissionsDict.REQUEST_ADITIONAL_ENROLLMENT,
-  //       EnrollmentPermissionsDict.REVIEW_ADDITIONAL_ENROLLMENT,
-  //     ])
-  //   ) {
-  //     throw redirect({
-  //       to: '/',
-  //     })
-  //   }
-  // },
+  beforeLoad: ({ context: { sessionStore } }) => {
+    const { getAllPermissions } = sessionStore
+    if (
+      !haveSomePermission(
+        getAllPermissions(),
+        EnrollmentModule.submodules.find(
+          (submodule) => submodule.path === '/matricula/propuesta-horarios'
+        )!.permissions
+      )
+    ) {
+      throw redirect({
+        to: '/',
+      })
+    }
+  },
   validateSearch: () => ({}) as EnrollmentProposalFilters,
-  loader: async ({ context: { queryClient } }) => {
-    // TODO: Falta agarrar el faculty id de la sesion
+  loader: async ({ context: { queryClient, sessionStore } }) => {
+    const { getRoleWithPermission } = sessionStore
+    const unitId = getRoleWithPermission(
+      EnrollmentPermissionsDict.REQUEST_SCHEDULE_PROPOSAL
+    )!.unitId
     return queryClient.ensureQueryData({
       queryKey: [QueryKeys.enrollment.ENROLLMENT_PROPOSALS],
-      //TODO: Arreglar el filtro
-      queryFn: () => EnrollmentService.getEnrollmentProposals({ facultyId: 0, filters: {}}),
+      queryFn: () =>
+        EnrollmentService.getEnrollmentProposals({
+          facultyId: unitId,
+          filters: {},
+        }),
     })
   },
   component: () => <EnrollmentPropose />,
--- src/database/seed.ts
+++ src/database/seed.ts
@@ -28,6 +28,7 @@ import {
 import { UnitsInsertSchema } from './schema/units'
 import { BaseRoles } from '@/interfaces/enums/BaseRoles'
 import BaseModules, { BaseModulesDict } from '@/auth/modules'
+import { ThesisPermissions } from '@/auth/permissions'
 
 const queryClient = postgres({
   db: DB_DATABASE,
@@ -141,12 +142,12 @@ await db.transaction(async (tx) => {
   await tx.insert(roles).values([
     {
       name: 'Estudiante',
-      unitType: 'university',
+      unitType: 'speciality',
       editable: false,
     },
     {
       name: 'Profesor',
-      unitType: 'university',
+      unitType: 'section',
       editable: false,
     },
     {
@@ -244,23 +245,91 @@ await db.transaction(async (tx) => {
     },
   ])
   //Base account
-  const [{ accountId }] = await tx
+  const accountsId = await tx
     .insert(accounts)
-    .values({
-      name: 'Fabrizio Randall',
-      firstSurname: 'Montoya',
-      secondSurname: 'Pinto',
-      code: '20212486',
-      email: 'fmontoya@pucp.edu.pe',
-      unitId: universityId,
-    })
+    .values([
+      {
+        name: 'Fabrizio Randall',
+        firstSurname: 'Montoya',
+        secondSurname: 'Pinto',
+        code: '20212486',
+        email: 'fmontoya@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Ricardo Bartra',
+        firstSurname: 'Smith',
+        secondSurname: 'Bartra',
+        code: '20176243',
+        email: 'ricardo.bartra@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Leonardo Vega',
+        firstSurname: 'Grijalva',
+        secondSurname: 'Vega',
+        code: '20240102',
+        email: 'a20197102@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Sebastian Castillejo',
+        firstSurname: 'Franco',
+        secondSurname: 'Castillejo',
+        code: '20190948',
+        email: 'a20190948@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Piero Alvarez',
+        firstSurname: 'Castillo',
+        secondSurname: 'Alvarez',
+        code: '20195903',
+        email: 'alvarez.piero@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Diego Ancajima',
+        firstSurname: 'Diaz',
+        secondSurname: 'Ancajima',
+        code: '20202308',
+        email: 'a20202308@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Jhoyfer Melendez',
+        firstSurname: 'Torres',
+        secondSurname: 'Melendez',
+        code: '20203823',
+        email: 'jmelendezt@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Alonso Alvarado',
+        firstSurname: 'Eslava',
+        secondSurname: 'Alvarado',
+        code: '20180731',
+        email: 'aalvaradoe@pucp.edu.pe',
+        unitId: universityId,
+      },
+      {
+        name: 'Paul Espettia',
+        firstSurname: 'Rodríguez',
+        secondSurname: 'Espettia',
+        code: '20181395',
+        email: 'paul.espettia@pucp.edu.pe',
+        unitId: universityId,
+      },
+    ])
     .returning({ accountId: accounts.id })
 
-  await tx.insert(accountRoles).values({
-    accountId,
-    roleId: BaseRoles.ADMIN,
-    unitId: universityId,
-  })
+  await tx.insert(accountRoles).values(
+    accountsId.map(({ accountId }) => ({
+      accountId,
+      roleId: BaseRoles.ADMIN,
+      unitId: universityId,
+    }))
+  )
 
   const insertedModules = await tx
     .insert(modules)
@@ -327,11 +396,20 @@ await db.transaction(async (tx) => {
       }))
   )
 
-  await tx.insert(accountRoles).values({
-    accountId,
-    roleId,
-    unitId: universityId,
+  await tx.insert(rolePermissions).values({
+    permissionId: permissionsInserted.find(
+      (permission) => permission.permissionName === ThesisPermissions[0].name
+    )?.permissionId!,
+    roleId: BaseRoles.STUDENT,
   })
+
+  await tx.insert(accountRoles).values(
+    accountsId.map(({ accountId }) => ({
+      accountId,
+      roleId,
+      unitId: universityId,
+    }))
+  )
 })
 console.log('Seed end')
 queryClient.end()
