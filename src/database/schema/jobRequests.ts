7e9d60c882d5fe62b2c7543fb8ed147381810492
ribartra <69020755+ribartra@users.noreply.github.com>
2024-11-07 13:21:37 -0500

    fix: adding fk in job request

--- src/database/schema/jobRequests.ts
+++ src/database/schema/jobRequests.ts
@@ -1,4 +1,4 @@
-import { integer, serial, text, uuid } from 'drizzle-orm/pg-core'
+import { serial, text, uuid } from 'drizzle-orm/pg-core'
 import { schema } from '..'
 import { accounts } from './accounts'
 import { relations } from 'drizzle-orm'
@@ -10,17 +10,21 @@ import { courseHirings } from './courseHirings'
 export const jobRequests = schema.table('job_requests', {
   id: serial('id').primaryKey(),
   accountId: uuid('account_id').references(() => accounts.id),
-  courseHiringId: uuid('course_hiring_id').references(() => courseHirings.id),
   requirementUrl: text('requirement_url'),
   motivation: text('motivation'),
   state: jobRequestState('state').default('sent').notNull(),
   observation: text('observation'),
+  courseHiringId: uuid('course_hiring_id').references(() => courseHirings.id),
 })
 
 export const jobRequestsRelations = relations(jobRequests, ({ one }) => ({
   account: one(accounts, {
     fields: [jobRequests.accountId],
     references: [accounts.id],
+  }),
+  courseHirings: one(courseHirings, {
+    fields: [jobRequests.courseHiringId],
+    references: [courseHirings.id],
   }),
 }))
 
