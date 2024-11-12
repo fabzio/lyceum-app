b9afbd21695784c879c9f08f42b5f1c8e413e741
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-11-12 01:59:58 -0500

    feat(database): agregar roles en semilla

--- src/auth/permissions/StudyPlan.ts
+++ src/auth/permissions/StudyPlan.ts
@@ -1,18 +1,25 @@
+export const StudyPlanPermissionsDict = {
+  READ_STUDY_PLAN: 'Ver planes de estudio',
+  MANAGE_STUDY_PLAN: 'Administrar planes de estudio',
+  MANAGE_COURSES: 'Administrar cursos',
+  READ_COURSES: 'Ver cursos',
+} as const
+
 const StudyPlanPermissions = [
   {
-    name: 'READ_STUDY_PLAN',
+    name: StudyPlanPermissionsDict.READ_STUDY_PLAN,
     description: 'Ver planes de estudio',
   },
   {
-    name: 'MANAGE_STUDY_PLAN',
+    name: StudyPlanPermissionsDict.MANAGE_STUDY_PLAN,
     description: 'Administrar planes de estudio',
   },
   {
-    name: 'MANAGE_COURSES',
+    name: StudyPlanPermissionsDict.MANAGE_COURSES,
     description: 'Administrar cursos',
   },
   {
-    name: 'READ_COURSES',
+    name: StudyPlanPermissionsDict.READ_COURSES,
     description: 'Ver cursos',
   },
 ] as const
