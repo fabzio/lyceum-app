88b00c77d665ad1e103f7e25a9be4fdb06dd63f0
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-11-12 16:34:55 -0500

    feat: agregar faq a lista de modulos

--- frontend/src/modules/index.ts
+++ frontend/src/modules/index.ts
@@ -6,6 +6,7 @@ import { UserModule } from './users/users.module'
 import { StudentProcessModule } from './student-process/student-process.module'
 import { EnrollmentModule } from './enrollment/enrollment.module'
 import { UnitModule } from './unit/unit.module'
+import { FAQModule } from './faq/faq.module'
 
 const Modules = [
   StudyPlanModule,
@@ -14,6 +15,7 @@ const Modules = [
   SecurityModule,
   UserModule,
   ThesisModule,
+  FAQModule,
   UnitModule,
 ]
 
