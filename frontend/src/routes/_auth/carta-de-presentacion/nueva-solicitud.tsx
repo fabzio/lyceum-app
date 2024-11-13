16cf81addd75de03219f4d01b5ab35092917b9fb
paul <paul.espettia@pucp.edu.pe>
2024-11-12 19:00:39 -0500

    feat(carta-de-presentacion): crear rutas para modulo de carta de presentacion

---
+++ frontend/src/routes/_auth/carta-de-presentacion/nueva-solicitud.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+//
+export const Route = createFileRoute(
+  '/_auth/carta-de-presentacion/nueva-solicitud'
+)({
+  component: () => (
+    <div>Hello /_auth/carta-de-presentacion/nueva-solicitud!</div>
+  ),
+})
