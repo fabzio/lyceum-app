6e1eaf0afd54fed19c6ba16484ad8a847ba63e74
Jrrrafa <134810050+Jrrrafa@users.noreply.github.com>
2024-11-14 14:41:41 -0500

    feat(carta-de-presentacion): agregar las rutas para el detalle

--- frontend/src/routes/_auth/carta-de-presentacion_/carta.tsx
+++ frontend/src/routes/_auth/carta-de-presentacion_/carta.tsx
@@ -3,7 +3,6 @@ import { createFileRoute, Outlet } from '@tanstack/react-router'
 export const Route = createFileRoute('/_auth/carta-de-presentacion/carta')({
   component: () => (
     <>
-      <div>Hello /_auth/carta-de-presentacion/carta-de-presentacion!</div>
       <Outlet />
     </>
   ),
