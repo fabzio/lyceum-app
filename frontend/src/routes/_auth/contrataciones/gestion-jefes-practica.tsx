f2c3b9ff651d96dab0f8a7633950bcf655293684
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-11-21 19:50:13 -0500

    fix(jps): enlazar gestión de horarios a gestión de JPs

--- frontend/src/routes/_auth/contrataciones/gestion-jefes-practica.tsx
+++ frontend/src/routes/_auth/contrataciones/gestion-jefes-practica.tsx
@@ -1,8 +1,8 @@
-import ManagementPracticeHeads from '@frontend/modules/hiring/views/ManagementPracticeHeads'
+import ScheduleManagement from '@frontend/modules/student-process/views/ScheduleManagement'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
   '/_auth/contrataciones/gestion-jefes-practica'
 )({
-  component: () => <ManagementPracticeHeads />,
+  component: () => <ScheduleManagement />,
 })
