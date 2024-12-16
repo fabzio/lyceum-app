56685c42e27e179947cacaa6ff1574d42f89c3ae
AAlvaradoE <aalvaradoe@pucp.edu.pe>
2024-12-15 22:43:59 -0500

    feat: implementar registro de comité (feo)

--- src/modules/hiring/services/hiringSelection.service.ts
+++ src/modules/hiring/services/hiringSelection.service.ts
@@ -465,12 +465,17 @@ class HiringSelectionService implements HiringSelectionDAO {
       )
 
     if (verification.length == 0) {
-      const result = await db.insert(jobRequests).values({
+      await db.insert(jobRequests).values({
         accountId: params.candidateAccount,
         motivation: params.jrMotivation,
         courseHiringId: params.courseHiringId,
         requirementUrl: params.file,
         state: 'sent',
+      })
+      await db.insert(accountsPerHiring).values({
+        accountId: params.candidateAccount,
+        courseHiringId: params.courseHiringId,
+        hiringType: 'candidate',
       })
     }
   }
